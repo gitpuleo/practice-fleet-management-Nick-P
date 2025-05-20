604458af73706994190370d4ab1af7d713db0e91
nfp <nfpuleo@gmail.com>
2025-05-19 23:57:50 -0500

    Classes classes and inheritances inherited

--- class-task.js
+++ class-task.js
@@ -7,13 +7,30 @@ class Vehicle{
         this.mileage = mileage;
 
     }
-
+    getDetails(){
+        return `This ${this.type} is a ${this.year} ${this.make} "${this.model}" with ${this.mileage} miles.`;
+    }
+    mileageDriven(distance){
+        this.mileage = this.mileage + distance;
+    }
 }
 
 
-let tundra = new Vehicle(truck, "Toyota", "Tundra", 2007, 237000);
+let tundra = new Vehicle("truck", "Toyota", "Tundra", 2007, 237000);
 
-let harley = new Vehicle(motorcycle, "Harley Davidson", "Big orange one", 2021, 15000);
+let harley = new Vehicle("motorcycle", "Harley Davidson", "Big orange one", 2021, 15000);
 
-let wrxsti = new Vehicle(rallyCar, "Subaru", "Impreza", 2003, 318000);
+let wrxsti = new Vehicle("rally car", "Subaru", "Impreza", 2003, 318000);
+
+
+console.log("Vehicle Report:\n");
+
+harley.mileageDriven(2300);
+console.log(harley.getDetails());
+
+tundra.mileageDriven(42);
+console.log(tundra.getDetails());
+
+wrxsti.mileageDriven(77000);
+console.log(wrxsti.getDetails());
 
